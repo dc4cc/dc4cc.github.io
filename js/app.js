fd1a0f789ad65537ca4093662ea4bf6542214c5d
mantajdhillon <mantajdhillon91@gmail.com>
2023-11-15 13:16:59 -0800

    Added the parallax affect

--- js/app.js
+++ js/app.js
@@ -8,8 +8,16 @@ window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
+    let zValue = 200;
+
     parallax_el.forEach((el) => {
-        el.style.transform = `translateX(calc(0% + ${-xValue}px)) translateY(calc(0% + ${yValue}px))`;
+       let speedx = el.dataset.speedx; 
+       let speedy = el.dataset.speedy;
+        el.style.transform = `translateX(calc(0% + ${-xValue * speedx}px)) 
+        translateY(calc(0% + ${yValue * speedy}px)) 
+        translateZ(${zValue}px)`;
     });
 
 });
+
+// perspective(2300px)
